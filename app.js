75554ba22a5826413e1be6af09030d06bb0db6bc
KarulinAV <aw.karulin@gmail.com>
2022-03-30 17:54:27 +0300

    Rendering Database Items in the ToDoList

--- app.js
+++ app.js
@@ -23,16 +23,22 @@ const item2 = new Item({name: "Hit the + button to add a new item"});
 const item3 = new Item({name: "<-- Hit this to delete an item"});
 
 const defaultItems = [item1, item2, item3];
-Item.insertMany(defaultItems, function(err){
-  if(err){
-    console.log(err);
-  }else{
-    console.log("Successfully saved default items to DB");
-  }
-});
 
 app.get('/', function(req, res) {
-  res.render("list", {listTitle: "Today", newListItems: items});
+  Item.find({}, function(err, foundItems){
+    if (foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          console.log("Successfully saved default items to DB");
+        }
+      });
+      res.redirect('/');
+    }else{
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  });
 });
 
 app.post('/', function(req, res) {
